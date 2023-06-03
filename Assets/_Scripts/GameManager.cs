52db23179fc85798c4a550e6d2b931ff2192b011
BleuSky03 <taksin@photomann.com>
2023-06-03 12:43:24 -0400

    UI + Import Textmeshpro
    Adding basic UI for game for timer text and telling player what event will happen at the end of the game.

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 
 public class GameManager : Singleton<MonoBehaviour>
 {
-    public float timeLeft;
+    
     [SerializeField] private TextMeshProUGUI gameText;
-    [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private TextMeshProUGUI timerText;
     private EndState endState;
+
+    
+    [Header("Timer")]
+    public float timeLeft;
+    [SerializeField] private float colorTransitionSpeed;
+    //in the green
+    [SerializeField] private Color green;
+    //in the orange
+    [SerializeField] private Color orange;
+    [SerializeField] private float orangeThreshold;
+    //in the red
+    [SerializeField] private Color red;
+    [SerializeField] private float redThreshold;
 
     public enum EndState
     {
@@ -27,11 +40,27 @@ public class GameManager : Singleton<MonoBehaviour>
         
     }
 
-    IEnumerator GameTimer()
+    IEnumerator Gametimer()
     {
         while (true)
         {
             gameText.text = "Time left until" + endState + " : ";
+
+            timeLeft = timeLeft - Time.deltaTime;
+            timerText.text = timeLeft.ToString("F" + 2);
+            if (timeLeft < redThreshold)
+            {
+                timerText.color = Color.Lerp(timerText.color, red, Time.deltaTime * colorTransitionSpeed);
+            }
+            else if (timeLeft < orangeThreshold)
+            {
+                timerText.color = Color.Lerp(timerText.color, orange, Time.deltaTime * colorTransitionSpeed);
+            }
+            else
+            {
+                timerText.color = Color.Lerp(timerText.color, green, Time.deltaTime * colorTransitionSpeed);
+            }
+
             yield return null;
         }
     }
