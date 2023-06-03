15c9c01b0b514cb10344c65ab29f4d17fa876464
Sam Morrow <notsammorrow@gmail.com>
2023-06-03 13:26:27 -0400

    walking anim controller

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    [SerializeField] private GameObject _playerBody;
     private Rigidbody2D rb;
-
+    private Animator playerAnimator;
+        
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerAnimator = _playerBody.GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -17,6 +20,16 @@ public class PlayerMovement : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
         Vector2 vel = new Vector2(xInput, yInput).normalized * speed;
+
+        if (Mathf.Abs(xInput) > 0 || Mathf.Abs(yInput) > 0)
+        {
+            playerAnimator.SetBool("IsWalking", true);
+        } else
+        {
+            playerAnimator.SetBool("IsWalking", false);
+        }
+
         rb.MovePosition(rb.position + vel * Time.deltaTime);
+
     }
 }
