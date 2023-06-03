045c5965bd410093a51c1600124a2a989cf2d9a5
Sam Morrow <notsammorrow@gmail.com>
2023-06-03 12:08:39 -0400

    add scripts directory, singleton

---
+++ Assets/_Scripts/Utilities/Singleton.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+/// <summary>
+/// A singleton that overrides its current instance. Handy for resetting state.
+/// </summary>
+public abstract class StaticInstance<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+    protected virtual void Awake() => Instance = this as T;
+
+    protected virtual void OnApplicationQuit()
+    {
+        Instance = null;
+        Destroy(gameObject);
+    }
+}
+
+/// <summary>
+/// This will destroy itself if any static instance already exists, effectively letting
+/// the original instance remain intact.
+/// </summary>
+
+public abstract class Singleton<T> : StaticInstance<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        base.Awake();
+    }
+}
+
+/// <summary>
+/// Survives scene loads. Good for audio sources or classes that hold persistent, stateful data.
+/// </summary>
+public abstract class SingletonPersistent<T> : Singleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+}
